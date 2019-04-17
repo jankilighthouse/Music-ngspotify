8cddffcc724fe69450f6684a7e66af1822349744
jankilighthouse <jankipatel6890@gmail.com>
2019-04-17 21:47:32 +0000

    navbar component, about component created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,14 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from "./components/navbar/navbar.component";
+
+import {Http, Response, Headers, RequestOptions} from '@angular/http';
 
 @Component({
+  moduleId: module.id,
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  templateUrl: 'app.component.html',
+
+
 })
-export class AppComponent  { name = 'Angular'; }
+
+export class AppComponent  {  }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NavbarComponent } from "./components/navbar/navbar.component";
 
 import { AppComponent }  from './app.component';
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent,NavbarComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
